66046b959d3de3aa53cc5ebdd5bbb1b6b85f6141
Raunak Agrawal <raunk325@gmail.com>
2018-07-14 12:39:17 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require ('express');
+const fs = require('fs');
+const hbs = require('hbs');
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log',log +'\n',(err) =>{
+		if(err){
+			console.log('cant do it');
+		}
+		
+	});
+	next();
+});
+	
+
+
+// app.use((req,res,next) => {
+// 	res.render('maintainence');
+
+
+// });
+
+hbs.registerHelper('currentYear',() => {
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamit', (text) => {
+	return text.toUpperCase();
+
+});
+
+app.get('/',(req,res) => {
+
+	res.render('home',{
+		pagetitle : 'About page',
+		welcome : 'Welcome here',
+	});
+
+});
+app.get('/about',(req,res) => {
+	res.render('about',{
+		pagetitle : 'About page',	
+	});
+});
+app.listen(3000);
